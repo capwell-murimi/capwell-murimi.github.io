8215edeb9037d57d106311570c44db07c4d6dfd1
capwell-murimi <capwellmurimi@gmail.com>
2025-08-11 14:19:55 +0300

    Setup GitHub Pages deploy for Vite + TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: 'https://github.com/capwell-murimi/capwell-murimi.github.io',
   server: {
     host: "::",
     port: 8080,
